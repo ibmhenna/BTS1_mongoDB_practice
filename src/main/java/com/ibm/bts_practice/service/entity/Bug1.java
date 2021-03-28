b4cd3d8a53d5d11aaad429c2d006b9f28d03acf2
unknown <ibmjavanh11@iiht.tech>
2021-03-29 00:29:38 +0530

    added verification for type, status and severity

--- src/main/java/com/ibm/bts_practice/service/entity/Bug1.java
+++ src/main/java/com/ibm/bts_practice/service/entity/Bug1.java
@@ -31,6 +31,16 @@ public class Bug1 {
 	}
 
 	public void setType(String type) {
+		int count = 0;
+		String[] arr = { "compile time", "syntax", "run time", "logical", "arithmetic" };
+		for (int i = 0; i < arr.length; i++) {
+			if (status.equals(arr[i])) {
+				count++;
+			}
+		}
+		if (count == 0) {
+			throw new IllegalArgumentException("Invalid Type");
+		}
 		this.type = type;
 	}
 
@@ -39,6 +49,17 @@ public class Bug1 {
 	}
 
 	public void setStatus(String status) {
+		int count = 0;
+		String[] arr = { "New", "Assigned", "Open", "Reopen", "Closed", "Verified", "Fixed", "PendingRequest",
+				"Retest" };
+		for (int i = 0; i < arr.length; i++) {
+			if (status.equals(arr[i])) {
+				count++;
+			}
+		}
+		if (count == 0) {
+			throw new IllegalArgumentException("Invalid Status");
+		}
 		this.status = status;
 	}
 
@@ -47,6 +68,16 @@ public class Bug1 {
 	}
 
 	public void setSeverity(String severity) {
+		int count = 0;
+		String[] arr = { "mild", "low", "severe" };
+		for (int i = 0; i < arr.length; i++) {
+			if (status.equals(arr[i])) {
+				count++;
+			}
+		}
+		if (count == 0) {
+			throw new IllegalArgumentException("Invalid Status");
+		}
 		this.severity = severity;
 	}
 
